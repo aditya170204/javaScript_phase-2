c4d72bee3786530acc46d2d352ec3ab4055bf128
aditya170204 <adityaraj17feb@gmail.com>
2024-12-22 12:47:09 +0530

    just revised the all basics

---
+++ strings.js
@@ -0,0 +1,33 @@
+// //////////////============== DATA TYPE CONVERSION ===========/////////////
+
+// let someNo = 30;
+// let someNO2 = String(someNo);
+// console.log(typeof someNO2);
+// console.log(typeof someNo);
+
+// //
+
+// let value = 3;
+// let negVal = -value
+// console.log(negVal)
+
+//
+
+// const someName = new String('javaScript')
+
+// console.log(someName[4])        // find by index
+// console.log(someName.__proto__)
+// console.log(someName.length)             //find the length
+// console.log(someName.toUpperCase())          // capitalize the value
+// console.log(someName.toLowerCase())             // lowercase the value
+// console.log(someName.indexOf('a'))
+// console.log(someName.charAt(6))
+
+/////
+
+const warName = "vibgyor"
+const newWars = warName.substring(0,4)
+const newWars2 = warName.slice(0,4)
+console.log(newWars)
+console.log(newWars2)
+console.log(warName)
